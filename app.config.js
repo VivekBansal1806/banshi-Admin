1e172c6504d1400510964f00098fa4ed31cfbdd3
VivekBansal <kumarv98133@gmail.com>
2025-08-25 05:21:10 +0530

    change app config

--- app.config.js
+++ app.config.js
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
   expo: {
@@ -6,30 +6,34 @@ export default {
     slug: "banshi-Admin",
     version: "1.0.0",
     orientation: "portrait",
-    icon: "./assets/icon.png",
+    icon: "./assets/icons/icon.png", // ✅ single icon
     userInterfaceStyle: "light",
     splash: {
       image: "./assets/splash-icon.png",
       resizeMode: "contain",
-      backgroundColor: "#ffffff"
+      backgroundColor: "#ffffff",
     },
-    assetBundlePatterns: [
-      "**/*"
-    ],
+    assetBundlePatterns: ["**/*"],
     ios: {
-      supportsTablet: true
+      supportsTablet: true,
     },
     android: {
+      package: "com.banshi.admin",
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#ffffff"
-      }
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
     },
     web: {
-      favicon: "./assets/favicon.png"
+      favicon: "./assets/favicon.png",
     },
     extra: {
-      apiBaseUrl: process.env.API_BASE_URL || 'https://banshi-fe4m.onrender.com',
+      apiBaseUrl:
+        process.env.API_BASE_URL || "https://banshi-fe4m.onrender.com",
+      eas: {
+        projectId: "9dae4eb3-36f8-4bb4-a826-3920df305b35",
+      },
     },
   },
-}; 
+};
